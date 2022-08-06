960b047d0fb9959c75c19e912f84c33e434a23d3
Kaka$hizuka <theshield310@gmail.com>
2022-08-06 15:58:22 +0700

    Add file DACN

---
+++ app/src/main/java/com/example/dacn/Logo_screen.java
@@ -0,0 +1,14 @@
+package com.example.dacn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Logo_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_screen);
+    }
+}
