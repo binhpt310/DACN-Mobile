21b8669a9bf4edeaa21d4f93d269b828f1d7824f
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-08-11 16:15:32 +0700

    lan 4

---
+++ app/src/main/java/com/example/dacn/thi_tracnghiem.java
@@ -0,0 +1,14 @@
+package com.example.dacn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thi_tracnghiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thi_tracnghiem);
+    }
+}
