7622f47bfff1a7de215f2cbbcc28617835e8fd9c
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-08-17 22:05:14 +0700

    xong dl tuan 2
    đặt tên + icon app
    code xong trang flash
    trang home + 2 trang trắc nghiệm

---
+++ app/src/main/java/com/example/dacn/trangchu.java
@@ -0,0 +1,36 @@
+package com.example.dacn;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class trangchu extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trangchu);
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout=findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        //setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+}
