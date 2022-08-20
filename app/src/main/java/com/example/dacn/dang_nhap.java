ff12936d61084438db5f6696494221c075c39eef
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-08-20 23:25:20 +0700

    edit xong dki dnhap

--- app/src/main/java/com/example/dacn/dang_nhap.java
+++ app/src/main/java/com/example/dacn/dang_nhap.java
@@ -2,13 +2,40 @@ package com.example.dacn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class dang_nhap extends AppCompatActivity {
 
+    Button btn_dangnhap;
+    TextView txt_dangky;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+
+        btn_dangnhap = findViewById(R.id.btn_dangnhap);
+        txt_dangky = findViewById(R.id.btn_dangkyotrangdangnhap);
+
+        btn_dangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dang_nhap.this, trangchu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        txt_dangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dang_nhap.this, dang_ky.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
