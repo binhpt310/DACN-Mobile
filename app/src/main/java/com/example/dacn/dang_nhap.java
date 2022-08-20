6c5611dc533fd57861ff90553fdb170f04a947c7
Kaka$hizuka <theshield310@gmail.com>
2022-08-20 12:27:44 +0700

    Add file dang nhap, dang ky

---
+++ app/src/main/java/com/example/dacn/dang_nhap.java
@@ -0,0 +1,14 @@
+package com.example.dacn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dang_nhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
