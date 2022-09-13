527671d222b7363c9face44f56f72e9d42f01a85
Kaka$hizuka <theshield310@gmail.com>
2022-09-14 00:26:30 +0700

    Update dangky test

--- app/src/main/java/com/example/dacn/dangnhap/dang_ky.java
+++ app/src/main/java/com/example/dacn/dangnhap/dang_ky.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.dacn.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class dang_ky extends AppCompatActivity {
 
     Button btn_dangky;
     TextView txt_dangnhap;
+    public boolean checkmk = true;
+    String txt1 = "";
+    String txt2 = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +33,27 @@ public class dang_ky extends AppCompatActivity {
 
         btn_dangky = findViewById(R.id.btn_dangky);
         txt_dangnhap = findViewById(R.id.dangnhapotrangdangky);
+        TextInputLayout nhapmk = findViewById(R.id.txt_nhapmatkhau);
+        TextInputLayout nhaplaimk = findViewById(R.id.txt_nhaplaimatkhau);
 
-        btn_dangky.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(dang_ky.this, dang_nhap.class);
-                startActivity(intent);
-                finish();
+        nhapmk.getEditText().addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //get the String from CharSequence with s.toString() and process it to validation
+                txt1 = s.toString();
             }
         });
+
+        nhaplaimk.getEditText().addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //get the String from CharSequence with s.toString() and process it to validation
+                txt2 = s.toString();
+            }
+        });
+
 
         txt_dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,5 +63,22 @@ public class dang_ky extends AppCompatActivity {
                 finish();
             }
         });
+
+        btn_dangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txt1 != txt2) {
+                    Toast.makeText(dang_ky.this, "Mật khẩu bạn nhập không giống nhau", Toast.LENGTH_SHORT).show();
+                    checkmk = false;
+                }
+                if  (txt1 == txt2){
+                    Intent intent = new Intent(dang_ky.this, dang_nhap.class);
+                    checkmk =true;
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
+
 }
