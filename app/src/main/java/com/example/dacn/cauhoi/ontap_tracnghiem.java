c3c198f8f3df81400a2c821eabfcd50e8c13be53
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-09-27 14:44:10 +0700

    update 2
    xong phần chuyển câu hỏi + input số câu

--- app/src/main/java/com/example/dacn/cauhoi/ontap_tracnghiem.java
+++ app/src/main/java/com/example/dacn/cauhoi/ontap_tracnghiem.java
@@ -12,7 +12,10 @@ import android.widget.Toast;
 import com.example.dacn.RetrofitInterface;
 import com.example.dacn.R;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,16 +25,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ontap_tracnghiem extends AppCompatActivity {
 
-    TextView cauhoi, cautraloia, cautraloib, cautraloic, cautraloid, xemnhanh;
+    TextView xemnhanh, socau;
+    TextView[] ar_textview = new TextView[5];
+
+    String[] ar_string = new String[6];
 
     public int Cauhoihientai = 0;
 
-    String cauHoi;
-    String cauA;
-    String cauB;
-    String cauC;
-    String cauD;
-    String anw ;
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,43 +48,31 @@ public class ontap_tracnghiem extends AppCompatActivity {
             public void onResponse(Call<List<CauHoiTracNghiem>> call, Response<List<CauHoiTracNghiem>> response) {
                 List<CauHoiTracNghiem> adslist = response.body();
 
-                cauHoi = adslist.get(Cauhoihientai).getQuestion();
-                cauA = adslist.get(Cauhoihientai).getA();
-                cauB = adslist.get(Cauhoihientai).getB();
-                cauC = adslist.get(Cauhoihientai).getC();
-                cauD = adslist.get(Cauhoihientai).getD();
-                anw = adslist.get(Cauhoihientai).getAnw();
-
-                cauhoi.setText(cauHoi);
-                cautraloia.setText(cauA);
-                cautraloib.setText(cauB);
-                cautraloic.setText(cauC);
-                cautraloid.setText(cauD);
+                gan_gia_tri(adslist,ar_string,ar_textview);
 
                 xemnhanh.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
                         Cauhoihientai++;
+                        Log.e("ee", String.valueOf(Cauhoihientai));
 
                         if (Cauhoihientai < (adslist.size())){
-                            cauHoi = adslist.get(Cauhoihientai).getQuestion();
-                            cauA = adslist.get(Cauhoihientai).getA();
-                            cauB = adslist.get(Cauhoihientai).getB();
-                            cauC = adslist.get(Cauhoihientai).getC();
-                            cauD = adslist.get(Cauhoihientai).getD();
-                            anw = adslist.get(Cauhoihientai).getAnw();
+                            gan_gia_tri(adslist,ar_string,ar_textview);
                         } else {
                             xemnhanh.setOnClickListener(null);
                         }
-
-                        cauhoi.setText(cauHoi);
-                        cautraloia.setText(cauA);
-                        cautraloib.setText(cauB);
-                        cautraloic.setText(cauC);
-                        cautraloid.setText(cauD);
                     }
                 });
+
+                /*for (int i=1;i<5;i++) {
+                    ar_textview[].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ktradungsai(ar_textview[],ar_string[]);
+                        }
+                    });
+                }*/
 
                 /*cautraloia.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -179,12 +168,63 @@ public class ontap_tracnghiem extends AppCompatActivity {
     }
 
     private void khaibao() {
-        cauhoi = findViewById(R.id.inputon_cauhoi);
-        cautraloia = findViewById(R.id.oncautraloia);
-        cautraloib = findViewById(R.id.oncautraloib);
-        cautraloic = findViewById(R.id.oncautraloic);
-        cautraloid = findViewById(R.id.oncautraloid);
+        ar_textview[0] = findViewById(R.id.inputon_cauhoi);
+        ar_textview[1] = findViewById(R.id.oncautraloia);
+        ar_textview[2] = findViewById(R.id.oncautraloib);
+        ar_textview[3] = findViewById(R.id.oncautraloic);
+        ar_textview[4] = findViewById(R.id.oncautraloid);
         xemnhanh = findViewById(R.id.btn_on_xemnhanh);
+        socau = findViewById(R.id.txt_on_socauhoi);
     }
+
+    private void gan_gia_tri(List<CauHoiTracNghiem> a, String arg[], TextView tv[]) {
+        arg[0] = a.get(Cauhoihientai).getQuestion();
+        arg[1] = a.get(Cauhoihientai).getA();
+        arg[2] = a.get(Cauhoihientai).getB();
+        arg[3] = a.get(Cauhoihientai).getC();
+        arg[4] = a.get(Cauhoihientai).getD();
+        arg[5] = a.get(Cauhoihientai).getAnw();
+
+        tv[0].setText(arg[0]);
+
+        //random trùng
+        for (int i=1;i<5;i++) {
+            int value = random.nextInt((5 - 1) + 1) + 1;
+            tv[i].setText(arg[value]);
+        }
+
+        /*tv[1].setText(arg[1]);
+        tv[2].setText(arg[2]);
+        tv[3].setText(arg[3]);
+        tv[4].setText(arg[4]);*/
+
+        socau.setText(String.valueOf(Cauhoihientai+1));
+    }
+
+
+    /*private void ktradungsai(TextView a[], String arg[]) {
+        for (int i=1; i<4; i++) {
+            int j=i;
+            if (arg[i].equals(arg[5])) {
+                a[j].setBackgroundResource(R.drawable.bg_otracnghiem_xanh);
+            }
+            }
+        }*/
+        /*if (cauA.equals(arg[5])) {
+            a.setBackgroundResource(R.drawable.bg_otracnghiem_xanh);
+        } else {
+            a.setBackgroundResource(R.drawable.bg_otracnghiem_do);
+            if (cauB.equals(arg[5])) {
+                b.setBackgroundResource(R.drawable.bg_otracnghiem_xanh);
+            } else if (cauC.equals(arg[5])) {
+                c.setBackgroundResource(R.drawable.bg_otracnghiem_xanh);
+            } else if (cauD.equals(arg[5])) {
+                d.setBackgroundResource(R.drawable.bg_otracnghiem_xanh);
+            }
+        }
+        b.setOnClickListener(null);
+        c.setOnClickListener(null);
+        d.setOnClickListener(null);*/
+    //}
 
 }
